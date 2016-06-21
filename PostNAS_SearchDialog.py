322ea8970009754b51eeb6a6b817343feb871146
Marvin Brandt <marvin.brandt@kreis-unna.de>
2016-06-21 15:33:21 +0200

    Header changed

--- PostNAS_SearchDialog.py
+++ PostNAS_SearchDialog.py
@@ -7,12 +7,12 @@
     copyright          : (C) 2016 by Kreis-Unna
     email                : marvin.brandt@kreis-unna.de
  ***************************************************************************
- *                                                                                                                                    *
- *   This program is free software; you can redistribute it and/or modify                                       *
- *   it under the terms of the GNU General Public License as published by                                      *
- *   the Free Software Foundation; either version 2 of the License, or                                          *
- *   (at your option) any later version.                                                                                    *
- *                                                                                                                                    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
  ***************************************************************************/
 """
 
